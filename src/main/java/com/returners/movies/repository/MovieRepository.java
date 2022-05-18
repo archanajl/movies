826722d855fd0cac99bd5e6f8aad17236911905d
Archana Janarthanan <archanajl@gmail.com>
2022-05-18 06:38:19 +0100

    Refactor the fix for Search By actor issue with null values. Swagger code included.

--- src/main/java/com/returners/movies/repository/MovieRepository.java
+++ src/main/java/com/returners/movies/repository/MovieRepository.java
@@ -17,12 +17,12 @@ public interface MovieRepository extends JpaRepository<Movie,Long> {
             "INNER JOIN Certification  c ON m.certification.id = c.id " +
             "INNER JOIN Genre g ON m.genre.id = g.id "+
             "where m.id = :id or m.rating = :rating " +
-            "or array_to_string(actors, ',') like '%' || :actor || '%' " +
+            "or array_to_string(actors, ',') like '%' || cast(:actor as text) || '%' " +
             "or m.title = :title or m.year = :year " +
             "or c.id = :certificationId " +
             "or g.id = :genreId"
     )
-    List<Movie> findBySearchCriteriaWithActor(
+    List<Movie> findBySearchCriteria(
             @Param("id") Long id,
             @Param("rating") int rating,
             @Param("actor") String actor,
@@ -35,51 +35,16 @@ public interface MovieRepository extends JpaRepository<Movie,Long> {
     @Query( "select m from Movie m " +
             "INNER JOIN Certification  c ON m.certification.id = c.id " +
             "INNER JOIN Genre g ON m.genre.id = g.id "+
-            "where m.id = :id or m.rating = :rating " +
-            "or m.title = :title or m.year = :year " +
-            "or c.id = :certificationId " +
-            "or g.id = :genreId"
-    )
-    List<Movie> findBySearchCriteriaWithoutActor(
-            @Param("id") Long id,
-            @Param("rating") int rating,
-            @Param("title") String title,
-            @Param("year") int year,
-            @Param("certificationId") Long certificationId,
-            @Param("genreId") Long genreId
-    );
-
-    @Query( "select m from Movie m " +
-            "INNER JOIN Certification  c ON m.certification.id = c.id " +
-            "INNER JOIN Genre g ON m.genre.id = g.id "+
             "where (c.id IN :ids) AND ( m.id = :id or m.rating = :rating " +
-                "or array_to_string(actors, ',') like '%' || :actor || '%' " +
+                "or array_to_string(actors, ',') like '%' || cast(:actor as text) || '%' " +
                 "or m.title = :title or m.year = :year " +
                 "or c.id = :certificationId " +
                "or g.id = :genreId)"
     )
-    List<Movie> findBySearchCriteriaForUserWithActor(
+    List<Movie> findBySearchCriteriaForUser(
             @Param("id") Long id,
             @Param("rating") int rating,
             @Param("actor") String actor,
-            @Param("title") String title,
-            @Param("year") int year,
-            @Param("ids") Long[] ids,
-            @Param("certificationId") Long certificationId,
-            @Param("genreId") Long genreId
-    );
-
-    @Query( "select m from Movie m " +
-            "INNER JOIN Certification  c ON m.certification.id = c.id " +
-            "INNER JOIN Genre g ON m.genre.id = g.id "+
-            "where (c.id IN :ids) AND ( m.id = :id or m.rating = :rating " +
-            "or m.title = :title or m.year = :year " +
-            "or c.id = :certificationId " +
-            "or g.id = :genreId)"
-    )
-    List<Movie> findBySearchCriteriaForUserWithoutActor(
-            @Param("id") Long id,
-            @Param("rating") int rating,
             @Param("title") String title,
             @Param("year") int year,
             @Param("ids") Long[] ids,
